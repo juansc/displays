22bc21a7745228e24d3504cea415d12d98a519f5
Juan <juanscarrillo23@gmail.com>
2023-10-27 21:16:36 -0700

    more cleanup

--- src/main.rs
+++ src/main.rs
@@ -87,18 +87,17 @@ fn main() {
     let mut laptop_display_info: Option<DisplayInfo> = None;
     let mut external_monitors = vec![];
 
-    for i in 0..display_count {
-        let display_id = displays[i as usize];
+    for display_id in &displays {
+        let bounds = unsafe { CGDisplayBounds(*display_id) };
+        let info = DisplayInfo::new(*display_id, bounds);
 
-        let bounds = unsafe { CGDisplayBounds(display_id) };
-        let info = DisplayInfo::new(display_id, bounds);
-
-        if unsafe { CGDisplayIsBuiltin(display_id) } == 1 {
+        if unsafe { CGDisplayIsBuiltin(*display_id) } == 1 {
             laptop_display_info = Some(info);
         } else {
             external_monitors.push(info);
         }
-    }
+    } 
+
 
     if external_monitors.len() != 2 {
         println!(
